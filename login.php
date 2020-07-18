d8a163a809def58d31c976c140b9997cf179b398
adeyemionward <adeyemiadeshina6@gmail.com>
2020-07-18 11:32:52 +0100

    login, and registration eroe messages modified

--- login.php
+++ login.php
@@ -2,37 +2,39 @@
 session_start();
 if (!isset($_SESSION["email"])) {
 ?>
-<!DOCTYPE html>
-<html>
-<?php include_once("includes/head.php"); ?>
+  <!DOCTYPE html>
+  <html>
+  <?php include_once("includes/head.php"); ?>
 
-<body style="background-image: url(images/bg1.jpg);background-repeat: no-repeat; background-size:cover; background-position: center;background-attachment: fixed;">
+  <body style="background-image: url(images/bg1.jpg);background-repeat: no-repeat; background-size:cover; background-position: center;background-attachment: fixed;">
+    <!-- school registration -->
+    <div class="container">
+      <div class="col-lg-6 form-container offset-lg-3">
 
-  <!-- school registration -->
-  <div class="container">
-    <div class="col-lg-6 form-container offset-lg-3">
+        <form method="POST" id="login_school">
+          <h2 class="text-center" style="padding-top: 15px;">Login</h2>
 
-      <form method="POST" id="login_school">
-        <h2 class="text-center" style="padding-top: 15px;">Login</h2>
+          <div class="form-group col-lg-10 offset-lg-1">
+            <input type="email" class="form-control" required placeholder="Enter Email" name="email">
+          </div>
 
-        <div class="form-group col-lg-10 offset-lg-1">
-          <input type="email" class="form-control" required  placeholder="Enter Email" name="email">
-        </div>
+          <div class="form-group col-lg-10 offset-lg-1">
+            <label for="password">Password:</label>
+            <input type="password" class="form-control" required placeholder="Enter password" name="password">
+          </div>
+          <!-- output error messages -->
+          <div id="message" class="form-group col-lg-10 offset-lg-1"></div>
 
-        <div class="form-group col-lg-10 offset-lg-1">
-          <label for="password">Password:</label>
-          <input type="password" class="form-control" required  placeholder="Enter password" name="password">
-        </div>
-        <div id="message"></div>
-        <p class="text-center">Don't have an account? <a href="register">Register</a></p>
-        <div class="form-group col-lg-10 offset-lg-1">
-          <button type="submit" name="login" class="btn btn-success form-control">Login</button><br><br><br>
-        </div>
+          <p class="text-center">Don't have an account? <a href="register">Register</a></p>
+          <div class="form-group col-lg-10 offset-lg-1">
+            <button type="submit" name="login" class="btn btn-success form-control">Login</button><br><br><br>
+          </div>
 
-      </form>
+        </form>
+      </div>
     </div>
-  </div>
-  <?php include_once("includes/footer.php"); ?>
-<?php }else{
+    <?php include_once("includes/footer.php"); ?>
+  <?php } else {
+  //return to admin dashboard if session is set 
   echo "<script>window.location.href='admin-dashboard'</script>";
 } ?>
--- register.php
+++ register.php
@@ -5,8 +5,7 @@ if (!isset($_SESSION["email"])) {
   <!DOCTYPE html>
   <html>
 
-  <?php include_once("includes/head.php"); ?> 
-  ?>
+  <?php include_once("includes/head.php"); ?>
 
   <body style="background-image: url(images/bg1.jpg);background-repeat: no-repeat; background-size:cover; background-position: center;background-attachment: fixed;">
 
@@ -30,8 +29,9 @@ if (!isset($_SESSION["email"])) {
             <label for="password">Password:</label>
             <input type="password" class="form-control" id="pwd" required placeholder="Enter password" name="password">
           </div>
+          <!-- output error messages -->
           <div id="message" class="form-group col-lg-10 offset-lg-1"></div>
-          <p class="text-center">Already registered? <a href="register">Login</a></p>
+          <p class="text-center">Already registered? <a href="login">Login</a></p>
           <div class="form-group col-lg-10 offset-lg-1">
             <button type="submit" name="submit" class="btn btn-success form-control">Register School</button><br><br><br>
           </div>
@@ -41,5 +41,6 @@ if (!isset($_SESSION["email"])) {
     </div>
     <?php include_once("includes/footer.php"); ?>
   <?php } else {
+  //return to admin dashboard if session is set 
   echo "<script>window.location.href='admin-dashboard'</script>";
 } ?>
